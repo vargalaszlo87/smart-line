e3915c3e2e5a941fefc0040a97795e32525636dc
Laszlo Varga <varga.laszlo.drz@gmail.com>
2024-07-22 14:14:24 +0200

    process.hu
    signatures updating

--- process.h
+++ process.h
@@ -8,5 +8,11 @@ extern entityJob();
 #include <stddef.h>
 #include <stdbool.h>
 
+int generateID(int );
+bool sendItem(smartline* );
+bool takeItem(smartline* );
+bool materialHandler(smartline* );
+void process01(smartline* );
+
 
 #endif // PROCESS_H_INCLUDED
